4b9dc008259efa2878aa83709a3f00029256fdb5
Adunola Johnson Odetola <aduntobi7@gmail.com>
2021-01-31 14:20:16 +0100

    update  file

--- index.js
+++ index.js
@@ -10,41 +10,58 @@ const operators = document.querySelectorAll("[data-operator]");
 const numberButtons = document.querySelectorAll("[data-number]");
 const equalsKey = document.querySelector("[data-equals]");
 const del = document.querySelector("[data-clear]");
-
-
+const display = document.querySelector(".display");
 
 const calculator = new Calculator(
-    previousOperandTextElement,
-    currentOperandTextElement
-    
-  );
+  previousOperandTextElement,
+  currentOperandTextElement
+);
+
+const unSelectNumber = () => {
+  numberButtons.forEach((button) => {
+    button.classList.remove("number-select");
+  });
+};
 
 numberButtons.forEach((button) => {
   button.addEventListener("click", () => {
-      
+    del.classList.remove("del-select")
+    unSelectNumber()
+    clearbtn()
+    button.classList.add('number-select')
     calculator.appendNumber(button.innerText);
     calculator.updateDisplay();
     operators.forEach((k) => k.classList.remove("operation-select"));
   });
 });
-clearAll.addEventListener("click", () => {
 
+const clearbtn = ()=>{
+  clearAll.classList.remove("clear-select")
+}
+clearAll.addEventListener("click", () => {
+  clearAll.classList.add("clear-select")
   calculator.clear();
   calculator.updateDisplay();
+  unSelectNumber()
   operators.forEach((k) => k.classList.remove("operation-select"));
 });
+
 const unSelectOperator = () => {
   operators.forEach((button) => {
     button.classList.remove("operation-select");
   });
 };
 
+
 operators.forEach((button) => {
   button.addEventListener("click", () => {
-    
+    del.classList.remove("del-select")
+    unSelectNumber()
     unSelectOperator();
+    clearbtn()
     button.classList.add("operation-select");
     calculator.chooseOperand(button.innerHTML);
+    button.innerHTML = button.innerHTML;
     calculator.updateDisplay();
   });
 });
@@ -52,25 +69,33 @@ operators.forEach((button) => {
 operators.forEach((button) => {
   if (button.innerHTML === "%") {
     button.addEventListener("click", () => {
-        
       calculator.chooseOperand(button.innerHTML);
       calculator.calculate();
       calculator.updateDisplay();
+      clearbtn()
     });
   }
 });
 
-equalsKey.addEventListener("click", (button) => {
-    
+equalsKey.addEventListener("click", () => {
+  if (
+    currentOperandTextElement.innerHTML === "" ||
+    previousOperandTextElement.innerHTML === ""
+  )
+    return;
+
   calculator.calculate();
   calculator.updateDisplay();
+  unSelectNumber()
+  clearbtn()
 });
 
+
 del.addEventListener("click", () => {
-    
+  del.classList.add("del-select")
   calculator.delete();
+  unSelectNumber()
+  clearbtn()
   calculator.updateDisplay();
   operators.forEach((k) => k.classList.remove("operation-select"));
 });
-
-
